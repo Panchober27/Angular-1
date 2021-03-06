cdc7f4c8d72ffcf75704262d69f5e0c685519b1e
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-06 19:39:32 -0300

    Revert "Avance con productsCtontrollers"
    This reverts commit 3ba21c0466ff86e71666b018de13bc9c9bb076bc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabeceraComponent } from './cabecera/cabecera.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CabeceraComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
